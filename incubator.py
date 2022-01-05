e7fe6655392d5797674f6bcbe04bd3f528a8cb39
rbneum <46759950+rbneum@users.noreply.github.com>
2022-01-05 11:03:38 -0800

    Framework for code approach

--- incubator.py
+++ incubator.py
@@ -1,3 +1,22 @@
 
 # Here is where the code lives
 
+# Motivation: 
+# Green and blue spaces in urban areas provide refuge against hot temperatures
+# Areas in King County impacted by air and water pollution
+# Important to understand benefits and risks associated with existing and planned spaces for climate adaptation
+
+# Import Data
+# polygons representing parks + environmental layer (grid?)
+# ideally, enviornmental data is at higher resolution than park data
+# data will come in different projections --> will need to reconcile
+
+# Clip data to area of interest --> both layers
+# clip with a new polygon that is area of interest or bounding box of coordinates
+
+# intersect data layers. polygon of parks with envrionmental data
+
+# summarize environmental data by park
+
+# visualize results -- not clear what exactly.
+# map with park polygons color coded by environmental data measure?
