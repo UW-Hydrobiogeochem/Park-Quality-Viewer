439e3077ee504d5398fb24b4a9fd9c3f176b3ddf
Spencer Wood <spwood@uw.edu>
2022-01-12 07:37:57 -0800

    An additional polygon import, then example plots
    here is some code for inspecting data (tables and map overlays)

--- incubator.py
+++ incubator.py
@@ -6,6 +6,15 @@
 # Areas in King County impacted by air and water pollution
 # Important to understand benefits and risks associated with existing and planned spaces for climate adaptation
 
+# Import Libraries
+# not sure what happened, but when re-ran code with 'pip install geopandas' got SyntaxError
+# tried downloading and installing pip. Didn't fix problem
+# code does run without those two lines, with just importing geopandas and reading data
+#$ python get-pip.py
+#pip install geopandas # not sure if I have to do this eash time or only once
+import geopandas
+import matplotlib.pyplot as plt
+
 # Import Data
 # polygons representing parks + environmental layer (grid?)
 # ideally, enviornmental data is at higher resolution than park data
@@ -13,14 +22,17 @@
 # geopandas.org for importing shapefiles
 # https://geopandas.org/en/stable/docs/user_guide/io.html
 # next step (jan 5): Becca practice import with smaller data. json file of King County parks that Spencer has al
+parks = geopandas.read_file("Data/king_county_parks.geojson")
+aoi = geopandas.read_file("Data/aoi.geojson")
 
-# not sure what happened, but when re-ran code with 'pip install geopandas' got SyntaxError
-# tried downloading and installing pip. Didn't fix problem
-# code does run without those two lines, with just importing geopandas and reading data
-#$ python get-pip.py
-#pip install geopandas # not sure if I have to do this eash time or only once
-import geopandas # not sure if I have to do this eash time or only once
-parks = geopandas.read_file("../Incubator2022/Data/aoi.geojson")
+# Inspect Data
+# look at tabular data
+parks.head()
+aoi.head()
+# make a plot 
+base = parks.plot(color="green")
+aoi.boundary.plot(ax=base, color="black")
+plt.show()
 
 # Clip data to area of interest --> both layers
 # clip with a new polygon that is area of interest or bounding box of coordinates
