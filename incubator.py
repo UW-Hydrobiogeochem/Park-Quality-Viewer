600a03b73497d30950f9a9a162344b904e40e462
rbneum <rbneum@uw.edu>
2022-03-01 17:38:31 -0800

    completed calculation of number of each pop served by each park

--- incubator.py
+++ incubator.py
@@ -87,6 +87,10 @@ pm25 = pd.read_csv("data/pm25block_wa_2015.csv")
 # pm25.dtypes
 no2 = pd.read_csv("data/no2block_wa_2015.csv")
 
+# -------- parkshed polygons
+parkshed_walk = gpd.read_file("data/parksheds_walk10min.geojson")
+# (1435, 14)
+
 ######################################
 # ---------- align projections ------
 ######################################
@@ -106,6 +110,7 @@ parks = parks.to_crs(2927)
 water = water.to_crs(2927)
 aoi = aoi.to_crs(2927)
 block = block.to_crs(2927)
+parkshed_walk = parkshed_walk.to_crs(2927)
 # crs conversion does not change data values. only changes geometries
 
 #####################################################
@@ -116,43 +121,21 @@ block = block.to_crs(2927)
 parks_clip = parks # getting error when clipping, skip clip for now
 water_clip = water.clip(aoi)
 water305Assess_clip = water305Assess_clean.clip(aoi) 
+parkshed_walk_clip = parkshed_walk.clip(aoi)
 
-# ------- merge air quality data with census block group data
+# ------- merge air quality data with census block group data to has geo reference
 block_int = block.astype({'GEOID10':"int64"}) # converting data types for merge
 # block_int.dtypes
 block_pm25 = block_int.merge(pm25, left_on='GEOID10', right_on='block_fip')
 block_no2 = block_int.merge(no2, left_on='GEOID10', right_on='block_fip')
 
 # ---------- clip data with aoi
+block_int_clip = block_int.clip(aoi)
 block_pm25_clip = block_pm25.clip(aoi)
 block_no2_clip = block_no2.clip(aoi)
 
-############################################
-# ---------- make plots of data -----------
-############################################
-
-# ----------- make a plot of parks and water
-base = parks_clip.plot(color="green")
-water_clip.plot(ax=base,color="blue")
-water305Assess_toxic_water_clip.plot(ax=base,color="red")
-water305Assess_toxic_tissue_clip.plot(ax=base,color="orange")
-aoi.boundary.plot(ax=base,color="black")
-plt.show()
-
-# ------ plot pm25 quality data with colors showing air quality
-block_pm25_clip.plot(column='pred15',legend='true',
-    legend_kwds={'label': "King County PM 2.5 ug/m3 in 2015",
-    'orientation': "horizontal"})
-plt.show()
-
-# ------- plot of all water quality data
-base = parks_clip.plot(color="grey")
-water_clip.plot(ax=base,color="black")
-water305Assess_clip.plot(ax=base,column='CategoryCode',legend='true')
-plt.show()
-
 ########################################################################
-# -------- intersect data layers to get environmental data at each park
+# -------- intersect data layers to get environmental & pop data at each park
 ########################################################################
 
 # create a new dataframe called parks_environ that holds all the enviornmental data about each park
@@ -161,11 +144,6 @@ parks_environ = parks_clip.copy()
 # ---------------------------
 # ------- water quality data
 # ---------------------------
-# plan: buffer parks by small amount. find intersection with King County water or WEQ water
-# if intersects with WEQ water ... assign the park the highest score of any intersected data
-# if intersects with King County water but not WEQ ... assign it a park with unkonwn quality = 0
-# if intersects with neither .. assign it a park with no water = nan
-
 # create a new object that will hold buffered parks data and geometry. 
 # Had to start with park geodataframe and then change geometry to be buffered geometry
 # becuase when run buffer command, just get geometry out. No other data comes along.
@@ -181,7 +159,9 @@ parks_environ = parks_environ.merge(WQ_park_intersect_max, how='left', left_on='
 # intersect buffered parks wtih water data layer from King County
 water_park_intersect = parks_clip_buffer.overlay(water_clip,how='intersection')
 water_park_intersect_size = water_park_intersect.groupby('OBJECTID_1')['OBJECTID_2'].size()
+
 parks_environ = parks_environ.merge(water_park_intersect_size,how='left', left_on='OBJECTID',right_on='OBJECTID_1')
+
 # find parks that have water but no water quality data. Assign these parks a value of 0
 # parks with no water keep nan code
 for i in range(len(parks_environ)):
@@ -189,17 +169,13 @@ for i in range(len(parks_environ)):
         if not np.isnan(parks_environ.loc[i,'OBJECTID_2']):
             parks_environ.loc[i,'CatCodeNum']=0
 # drop the column of data used to find parks with water but with no WQ data.
-# information now stored in 'CatCodeNum' column
+# because information now stored in 'CatCodeNum' column
 parks_environ = parks_environ.drop(columns=['OBJECTID_2'])
-
-# print result
-base = parks_clip.plot(color='grey') 
-parks_environ.plot(ax=base, column='CatCodeNum',legend='true')
-plt.show()
 
 #-------------------------
 # ------ air quality data 
 #-------------------------
+# overlap parks data with air quality data
 pm25_park_intersect = parks_clip.overlay(block_pm25_clip,how='intersection')
 # parks_clip['OBJECTID'].is_unique # true = ID is applied to each park
 # pm25_park_intersect['OBJECTID'].is_unique # false = parks have multiple air quality data
@@ -209,6 +185,7 @@ no2_park_intersect = parks_clip.overlay(block_no2_clip,how='intersection')
 
 # ------------calculated area-averaged values for enviornmental data for each park
 # example: (A1*E1 + A2*E2)/(A1+A2)
+
 # find area of each intersection and join to the intersect object
 pm25_park_intersect = pm25_park_intersect.join(pm25_park_intersect.area.to_frame(name='intersect_Area'))
 no2_park_intersect = no2_park_intersect.join(no2_park_intersect.area.to_frame(name='intersect_Area'))
@@ -221,22 +198,86 @@ no2area = no2_park_intersect['intersect_Area'] * no2_park_intersect['pred15']
 pm25_park_intersect = pm25_park_intersect.join(pm25area.to_frame(name='pm25area'))
 no2_park_intersect = no2_park_intersect.join(no2area.to_frame(name='no2area'))
 
-# sum the multiplied values that exist within a given park and append to a new park object
+# sum the multiplied values that exist within a given park and merge with parks into to a new park object
 pm25_park_areasum = pm25_park_intersect.groupby('OBJECTID')['pm25area'].sum()
 no2_park_areasum = no2_park_intersect.groupby('OBJECTID')['no2area'].sum()
-pm25_park = parks_clip.merge(pm25_park_areasum,left_on='OBJECTID', right_on='OBJECTID')
-no2_park = parks_clip.merge(no2_park_areasum,left_on='OBJECTID', right_on='OBJECTID')
-# NOTE: question for Spencer: does it matter merge versus join? We use merge here and join below.
+park_tmp = parks_clip.copy()
+park_tmp = park_tmp.merge(pm25_park_areasum,how='left', left_on='OBJECTID', right_on='OBJECTID')
+park_tmp = park_tmp.merge(no2_park_areasum,how='left', left_on='OBJECTID', right_on='OBJECTID')
 
 # divide sum of multiplied values by total park area to get area weighted average
-pm25areaAvg = pm25_park['pm25area'].divide(pm25_park['SHAPE_Area'])
-no2areaAvg = no2_park['no2area'].divide(no2_park['SHAPE_Area'])
+pm25areaAvg = park_tmp['pm25area'].divide(park_tmp['SHAPE_Area'])
+no2areaAvg = park_tmp['no2area'].divide(park_tmp['SHAPE_Area'])
 
-# append area weighted averages to a new park object
+# join area weighted averages to a new park object
 parks_environ = parks_environ.join(pm25areaAvg.to_frame(name='pm25areaAvg'))
 parks_environ = parks_environ.join(no2areaAvg.to_frame(name='no2areaAvg'))
 
-# visualize data in a plot
+#-----------------------
+# --- Parkshed data
+#---------------------
+# intersect parkshed polygons with block census data
+parkshed_walk_block = parkshed_walk_clip.overlay(block_int_clip,how='intersection')
+# (127332, 52)
+# multiple census blocks within the parkshed. Need to calculate effective population
+
+# ------------calculated area-averaged values for population within each parkshed for each park
+# example: (A1*P1 + A2*P2)/(A1+A2)
+
+# NOTE: this calculation is not correct. Need to figure out what issue is
+
+# find area of each intersection and join to the intersect object
+parkshed_walk_block = parkshed_walk_block.join(parkshed_walk_block.area.to_frame(name='intersect_Area'))
+
+# multiply interesect area by population race for that area
+# join multiplied area to the intersect object
+poplist = ['POPHISP','POPWHITE2','POPBLACK2','POPAIAN2','POPASIAN2','POPNHOPI2','POPOTH2','POPTWO2']
+for pop in poplist:
+    calc = parkshed_walk_block['intersect_Area'] * parkshed_walk_block[pop]
+    colname = "Area_x_" + pop
+    parkshed_walk_block = parkshed_walk_block.join(calc.to_frame(name=colname))
+# sum multipled values with a given park and merage with parks into a temp park object
+for pop in poplist:
+    colname = "Area_x_" + pop
+    area = parkshed_walk_block.groupby('OBJECTID')[colname].sum()
+    park_tmp = park_tmp.merge(area,how='left',on='OBJECTID')
+# divide sum of multiplied values by total park area to get area weighted average
+# join area weighted averages to a parks_environ object
+for pop in poplist:
+    colname = "Area_x_" + pop
+    areaavg = park_tmp[colname].divide(park_tmp['SHAPE_Area'])
+    colname1 = 'Walk_' + pop
+    parks_environ = parks_environ.join(areaavg.to_frame(name=colname1))
+
+############################################
+# ---------- make plots of data -----------
+############################################
+
+# ----------- make a plot of parks and water
+base = parks_clip.plot(color="green")
+water_clip.plot(ax=base,color="blue")
+aoi.boundary.plot(ax=base,color="black")
+plt.show()
+
+# ------ plot pm25 quality data with colors showing air quality
+block_pm25_clip.plot(column='pred15',legend='true',
+    legend_kwds={'label': "King County PM 2.5 ug/m3 in 2015",
+    'orientation': "horizontal"})
+plt.show()
+
+# ------- plot of all water quality data
+base = parks_clip.plot(color="grey")
+water_clip.plot(ax=base,color="black")
+water305Assess_clip.plot(ax=base,column='CategoryCode',legend='true')
+plt.show()
+
+# plot water quality data assigned to parks 
+base = parks_clip.plot(color='grey') 
+water_clip.plot(ax=base,color='blue')
+parks_environ.plot(ax=base, column='CatCodeNum',legend='true')
+plt.show()
+
+# visualize air quality data assigned to parks in a plot
 parks_environ.plot(column='pm25areaAvg',legend='true',
     legend_kwds={'label': "Park Area-Weighted PM 2.5 ug/m3 in 2015",
     'orientation': "horizontal"})
@@ -247,12 +288,17 @@ parks_environ.plot(column='no2areaAvg',legend='true',
     'orientation': "horizontal"})
 plt.show()
 
-# summarize environmental data by park
-# can work directly on attributes and ignore geometry. e.g. take mean of polygon
-# produce a new geofile to display
-# can probably do this with pandas...will operate on attributes of polygons
-# spatial joins
+# visualize populations in walkable distance to parks
+# 8 populations. 2x4 subplot
+plt.figure(figsize=(15, 20))
+plt.subplots_adjust(hspace=0.5)
+loopnum = 0
+for pop in poplist:
+    loopnum = loopnum +1
+    colname = "Walk_" + pop
+    ax = plt.subplot(4,2,loopnum)
+    parks_environ.plot(ax=ax,column=colname,legend='true',legend_kwds={'label': 
+    colname, 'orientation': "horizontal"})
+plt.show()
+# plotting does not seem correct.
 
-# visualize results -- not clear what exactly.
-# map with park polygons color coded by environmental data measure?
-# Ask Scott about visualization step
